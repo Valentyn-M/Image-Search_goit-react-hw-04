411335ce194af496f73bb10af227679df281c9bb
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-11-24 18:55:01 +0100

    add zoom for img

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,7 +3,7 @@ import s from "./ImageCard.module.css"
 const ImageCard = ({ data }) => {
 
 	return (
-		<div className={s.imgWrap}>
+		<div className={s.imgWrap} tabIndex="0">
 			<img className={s.image} src={data.urls.small} alt={data.description ?? data.alt_description} width="300" height="200" loading="lazy" />
 		</div>
 	)
